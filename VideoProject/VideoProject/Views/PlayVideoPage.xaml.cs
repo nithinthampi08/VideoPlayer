845fe782892e8cb515ff4326a6b28cdf859e0208
Nithin Thampi <nithint@reflections.local>
2018-05-18 11:17:16 +0530

    FirstCommit

---
+++ VideoProject/VideoProject/Views/PlayVideoPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VideoProject.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PlayVideoPage : ContentPage
+	{
+		public PlayVideoPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
